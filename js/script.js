b8adbecaa9855409deb5ac6206a1bac16b851a3d
Zhibek Shakenova <zhibek.shakenovva.on@gmail.com>
2022-01-11 18:24:42 +0600

    add hw3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+// hw1
 // let userName = "Shakenova Zhibek";
 // console.log(userName);
 
 
-let userName = "Zhibek";
-let userSurname = ("Shakenova");
-console.log(userSurname);
+// hw2
+// let userName = "Zhibek";
+// let userSurname = ("Shakenova");
+// console.log(userSurname);
+
+
+// hw3
+let a = "My name is Zhibek. ";
+let b = " It's my Email. zhibek.shakenova.on@gmail.com";
+console.log(a+b);
+
+

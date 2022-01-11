64ba62b9bec272f7e45ebed4d85df8e23ca31cce
Zhibek Shakenova <zhibek.shakenovva.on@gmail.com>
2022-01-11 19:28:20 +0600

    add hw5

--- js/script.js
+++ js/script.js
@@ -15,8 +15,37 @@
 // console.log(a+b);
 
 // hw4
-let a = 75;
-let b = 112;
-console.log(a+b);
+// let a = 75;
+// let b = 112;
+// console.log(a+b);
 
 
+//hw5
+// let a = 10;
+// let b = 40;
+// console.log(a+b);
+
+
+
+
+let a = Number(prompt("введите число a"));
+
+let b = Number(prompt("введите число b"));
+
+let summa = a+b;
+alert("сумма = "+summa);
+
+let minus = a-b;
+alert("разница = "+minus);
+
+let multiplication = a*b;
+alert("произведение = "+multiplication);
+
+let divide = a/b;
+alert("частное от деления ="+divide);
+
+let remind = a%b;
+alert("остаток от деления ="+remind);
+
+let degree = a**b;
+alert("результат возведения числа a в степень b = "+degree);
